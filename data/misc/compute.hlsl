fcb702f0e106bbe0943bf3f33c823c81ff788a11
Vasko <magaranov@abv.bg>
2022-09-04 20:33:42 +0300

    preparing to actually do something in the compute shader

--- data/misc/compute.hlsl
+++ data/misc/compute.hlsl
@@ -1,4 +1,13 @@
+struct Col
+{
+	float4 color;
+};
+
+StructuredBuffer<PosVelo>	inData	: register(t0);
+RWStructuredBuffer<PosVelo>	outData	: register(u0);
+
 [numthreads(1, 1, 1)]
 void CSMain(uint3 Gid : SV_GroupID, uint3 DTid : SV_DispatchThreadID, uint3 GTid : SV_GroupThreadID, uint GI : SV_GroupIndex)
 {
+	outData[0] = float4(1,0,1,1);
 }
