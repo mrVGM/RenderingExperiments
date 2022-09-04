5b3a4485cff31a2814096588364cb9645d379ff5
Vasko <magaranov@abv.bg>
2022-09-04 23:08:26 +0300

    compute shader working

--- data/misc/compute.hlsl
+++ data/misc/compute.hlsl
@@ -4,5 +4,6 @@ RWStructuredBuffer<float4> outData	: register(u0);    // UAV
 [numthreads(1, 1, 1)]
 void CSMain(uint3 Gid : SV_GroupID, uint3 DTid : SV_DispatchThreadID, uint3 GTid : SV_GroupThreadID, uint GI : SV_GroupIndex)
 {
-	outData[0] = float4(1,1,1,1);
+	outData[0] = float4(0,0,0.5,0.5);
+	outData[1] = float4(1,1,0,1);
 }
