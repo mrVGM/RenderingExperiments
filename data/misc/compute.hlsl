4c4260722c6ece1cd3f9a20fc724a9d65520a5b6
Vasko <magaranov@abv.bg>
2022-09-06 16:35:29 +0300

    constant buffer of compute shader working

--- data/misc/compute.hlsl
+++ data/misc/compute.hlsl
@@ -1,8 +1,14 @@
 StructuredBuffer<float4> inData		: register(t0);    // SRV
 RWTexture2D<float4> outData					: register(u0);    // UAV
 
+cbuffer SceneConstantBuffer : register(b0)
+{
+    float4 color;
+    float4 padding[15];
+};
+
 [numthreads(1, 1, 1)]
 void CSMain(uint3 Gid : SV_GroupID, uint3 DTid : SV_DispatchThreadID, uint3 GTid : SV_GroupThreadID, uint GI : SV_GroupIndex)
 {
-	outData[DTid.xy] = float4(1, 1, 1, 1);
+	outData[DTid.xy] = color;
 }
--- data/misc/shaders.hlsl
+++ data/misc/shaders.hlsl
@@ -1,14 +1,3 @@
-//*********************************************************
-//
-// Copyright (c) Microsoft. All rights reserved.
-// This code is licensed under the MIT License (MIT).
-// THIS CODE IS PROVIDED *AS IS* WITHOUT WARRANTY OF
-// ANY KIND, EITHER EXPRESS OR IMPLIED, INCLUDING ANY
-// IMPLIED WARRANTIES OF FITNESS FOR A PARTICULAR
-// PURPOSE, MERCHANTABILITY, OR NON-INFRINGEMENT.
-//
-//*********************************************************
-
 struct PSInput
 {
     float4 position : SV_POSITION;
@@ -22,7 +11,7 @@ PSInput VSMain(float2 position : POSITION, float2 uv : UV)
 {
     PSInput result;
 
-    result.position = float4(0.5 * position, 0, 1);
+    result.position = float4(position, 0, 1);
     result.uv = uv;
 
     return result;
@@ -30,8 +19,5 @@ PSInput VSMain(float2 position : POSITION, float2 uv : UV)
 
 float4 PSMain(PSInput input) : SV_TARGET
 {
-    if (input.uv.x < 0.5) {
-        return p_texture.Sample(p_sampler, input.uv);
-    }
-    return float4(input.uv, 0, 1);
+    return p_texture.Sample(p_sampler, input.uv);
 }
