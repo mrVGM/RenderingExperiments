26da602eb4464e93941b0b949716df27d6d2ae68
Vasko <magaranov@abv.bg>
2022-09-06 18:11:49 +0300

    setting up the constant and structured compute buffers

--- data/misc/compute.hlsl
+++ data/misc/compute.hlsl
@@ -1,14 +1,16 @@
 StructuredBuffer<float4> inData		: register(t0);    // SRV
-RWTexture2D<float4> outData					: register(u0);    // UAV
+RWTexture2D<float4> outData			: register(u0);    // UAV
 
-cbuffer SceneConstantBuffer : register(b0)
+cbuffer InfoConstantBuff : register(b0)
 {
-    float4 color;
-    float4 padding[15];
+    int m_texSize;
+    int m_srvSize;
+
+    float m_padding[62];
 };
 
 [numthreads(1, 1, 1)]
 void CSMain(uint3 Gid : SV_GroupID, uint3 DTid : SV_DispatchThreadID, uint3 GTid : SV_GroupThreadID, uint GI : SV_GroupIndex)
 {
-	outData[DTid.xy] = color;
+	outData[DTid.xy] = float4(1,1,0,1);
 }
