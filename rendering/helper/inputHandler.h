6b55bbf69244af6773130492b7acf5640a6fbed9
mrVGM <maddvvas@gmail.com>
2022-10-31 17:31:06 +0200

    Update inputHandler.h

--- rendering/helper/inputHandler.h
+++ rendering/helper/inputHandler.h
@@ -1,1 +1,13 @@
-// inputHandler.h Placeholder
+#pragma once
+
+#include <Windows.h>
+#include <list>
+
+namespace rendering
+{
+	class InputHandler
+	{
+	public:
+		virtual void HandleInput(double dt, std::list<WPARAM>& keysDown, std::list<WPARAM>& keysUp) = 0;
+	};
+}
