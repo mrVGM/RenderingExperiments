d2ad267d426d706345df8943077a6325b1a486be
Vasko <magaranov@abv.bg>
2022-11-20 15:34:22 +0200

    copying data files using cmake

--- main/main.cpp
+++ main/main.cpp
@@ -7,12 +7,22 @@
 #include <filesystem>
 #include <iostream>
 
-int main()
+int main(int args, const char** argv)
 {
-	std::filesystem::path dataPath = std::filesystem::current_path().append("..\\..\\..\\..\\data\\");
+	std::string executableName = argv[0];
+	int index = executableName.find_last_of('\\');
+
+	if (index < 0) {
+		std::cerr << "Can't find data directory!" << std::endl;
+		return 1;
+	}
+	std::string executableDirectory = executableName.substr(0, index);
+	std::filesystem::path executableDirPath = std::filesystem::path(executableDirectory);
+
+	std::filesystem::path dataPath = executableDirPath.append("..\\data\\");
 	data::Init(dataPath.string().c_str());
 
-	std::filesystem::path scriptsDir = dataPath.append("misc\\");
+	std::filesystem::path scriptsDir = dataPath.append("scripts\\");
 
 	interpreter::ISession& session = interpreter::OpenSession(scriptsDir.string(), std::cout);
 	session.AddGlobalValue("api", rendering::GetAPI());
