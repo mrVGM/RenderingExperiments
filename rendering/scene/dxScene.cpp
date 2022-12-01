62249814e08c61df07a4a3f8efc3173b590da31e
Vasil <magaranov@abv.bg>
2022-12-01 17:44:10 +0200

    instance buffers into the scene class

--- rendering/scene/dxScene.cpp
+++ rendering/scene/dxScene.cpp
@@ -1,6 +1,8 @@
 #include "scene/dxScene.h"
 
 #include "nativeFunc.h"
+#include "utils.h"
+#include "dxBuffer.h"
 
 #include <list>
 
@@ -17,6 +19,8 @@ void rendering::scene::DXScene::InitProperties(interpreter::NativeObject & nativ
 #define THROW_EXCEPTION(error)\
 scope.SetProperty("exception", Value(error));\
 return Value();
+
+	Value& p_instanceBuffers = GetOrCreateProperty(nativeObject, "instanceBuffers");
 
 	Value& addObject = GetOrCreateProperty(nativeObject, "addObject");
 	addObject = CreateNativeMethod(nativeObject, 2, [](Value scope) {
@@ -177,8 +181,120 @@ return Value();
 		return Value();
 	});
 
+	Value& getInstanceBuffersData = GetOrCreateProperty(nativeObject, "getInstanceBuffersData");
+	getInstanceBuffersData = CreateNativeMethod(nativeObject, 0, [](Value scope) {
+		Value selfValue = scope.GetProperty("self");
+		DXScene* self = static_cast<DXScene*>(NativeObject::ExtractNativeObject(selfValue));
+		
+		self->ConstructInstanceBuffersData();
+
+		std::list<Value> tmp;
+		for (std::map<int, InstanceBuffer>::iterator it = self->m_instanceBuffers.begin(); it != self->m_instanceBuffers.end(); ++it) {
+			Value v = interpreter::utils::GetEmptyObject();
+			IManagedValue* managedValue = v.GetManagedValue();
+			managedValue->SetProperty("id", Value(it->first));
+			managedValue->SetProperty("count", Value(it->second.m_objectsCount));
+
+			tmp.push_back(v);
+		}
+
+		return Value::FromList(tmp);
+	});
+
+	Value& setInstanceBuffers = GetOrCreateProperty(nativeObject, "setInstanceBuffers");
+	setInstanceBuffers = CreateNativeMethod(nativeObject, 1, [&](Value scope) {
+		Value selfValue = scope.GetProperty("self");
+		DXScene* self = static_cast<DXScene*>(NativeObject::ExtractNativeObject(selfValue));
+
+		Value instanceBuffersValue = scope.GetProperty("param0");
+
+		p_instanceBuffers = instanceBuffersValue;
+
+		std::list<Value> instanceBuffers;
+		instanceBuffersValue.ToList(instanceBuffers);
+
+		for (std::list<Value>::iterator it = instanceBuffers.begin(); it != instanceBuffers.end(); ++it) {
+			Value cur = *it;
+			int id = static_cast<int>(cur.GetManagedValue()->GetProperty("id").GetNum());
+			Value buffValue = cur.GetManagedValue()->GetProperty("buff");
+
+			DXBuffer* buff = static_cast<DXBuffer*>(NativeObject::ExtractNativeObject(buffValue));
+
+			self->m_instanceBuffers[id].m_buffer = buff->GetBuffer();
+			self->m_instanceBuffers[id].m_size = buff->GetBufferWidth();
+			self->m_instanceBuffers[id].m_stride = buff->GetStride();
+		}
+
+		return Value();
+	});
+
 #undef THROW_EXCEPTION
+}
+
+void rendering::scene::DXScene::ConstructInstanceBuffersData()
+{
+	m_instanceBuffers.clear();
+
+	std::list<Object3D*> objectList;
+
+	for (std::map<std::string, Object3D>::iterator it = m_objects.begin(); it != m_objects.end(); ++it) {
+		objectList.push_back(&it->second);
+
+		it->second.m_instanceBufferID = -1;
+		it->second.m_instanceBufferOffset = -1;
+	}
+
+	for (std::list<Object3D*>::iterator testIt = objectList.begin(); testIt != objectList.end(); ++testIt) {
+		Object3D* tested = *testIt;
+
+		for (std::list<Object3D*>::iterator processedIt = objectList.begin(); processedIt != objectList.end(); ++processedIt) {
+			Object3D* processed = *processedIt;
+
+			if (tested->Similar(*processed)) {
+				tested->m_instanceBufferID = processed->m_instanceBufferID;
+
+				tested->m_instanceBufferOffset = m_instanceBuffers[tested->m_instanceBufferID].m_objectsCount;
+				++m_instanceBuffers[tested->m_instanceBufferID].m_objectsCount;
+				break;
+			}
+		}
+
+		if (tested->m_instanceBufferID < 0) {
+			int id = m_instanceBufferCount++;
+
+			InstanceBuffer instanceBuff;
+			instanceBuff.m_objectsCount = 1;
+
+			m_instanceBuffers.insert(std::pair<int, InstanceBuffer>(id, instanceBuff));
+			tested->m_instanceBufferID = id;
+			tested->m_instanceBufferOffset = 0;
+		}
+	}
 }
 
 
 #undef THROW_ERROR
+
+bool rendering::scene::Object3D::Similar(const Object3D& other) const
+{
+	if (m_mesh != other.m_mesh) {
+		return false;
+	}
+
+	if (m_materials.size() != other.m_materials.size()) {
+		return false;
+	}
+
+	std::list<std::string>::const_iterator matIt1 = m_materials.begin();
+	std::list<std::string>::const_iterator matIt2 = other.m_materials.begin();
+
+	while (matIt1 != m_materials.end()) {
+		if (*matIt1 != *matIt2) {
+			return false;
+		}
+
+		++matIt1;
+		++matIt2;
+	}
+	return true;
+}
--- rendering/scene/dxScene.h
+++ rendering/scene/dxScene.h
@@ -2,6 +2,7 @@
 
 #include "nativeObject.h"
 
+#include <d3d12.h>
 #include <DirectXMath.h>
 #include <map>
 #include <string>
@@ -17,15 +18,33 @@ namespace rendering::scene
 
 	struct Object3D
 	{
+		int m_instanceBufferID = -1;
+		int m_instanceBufferOffset = -1;
+
 		std::string m_mesh;
 		Transform m_transform;
 		std::list<std::string> m_materials;
+
+		bool Similar(const Object3D& other) const;
+	};
+
+	struct InstanceBuffer
+	{
+		int m_size = 0;
+		int m_stride = 0;
+		ID3D12Resource* m_buffer = nullptr;
+		int m_objectsCount = 0;
 	};
 
 	class DXScene : public interpreter::INativeObject
 	{
+		int m_instanceBufferCount = 0;
 		std::map<std::string, Object3D> m_objects;
+		std::map<int, InstanceBuffer> m_instanceBuffers;
+
 		void InitProperties(interpreter::NativeObject& nativeObject) override;
+
+		void ConstructInstanceBuffersData();
 	public:
 	};
 }
