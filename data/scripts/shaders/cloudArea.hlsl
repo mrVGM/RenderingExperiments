4b4df2b9eeec75d97434842d2de81a5e478d6963
Vasko <magaranov@abv.bg>
2022-12-21 15:52:21 +0200

    cloud area shader refactor

--- data/scripts/shaders/cloudArea.hlsl
+++ data/scripts/shaders/cloudArea.hlsl
@@ -13,18 +13,18 @@ cbuffer CloudAreaSettings : register(b1)
     float3 m_lightPosition;
     float m_lightIntensity;
 
-    float m_randomOffsetStrength;
-    float m_sampleSteps;
     float m_cloudAbsorbtion;
-    float m_densityOffset;
-
-    float m_cloudLightAbsorbtion;
     float m_airLightAbsorbtion;
+    float m_cloudLightAbsorbtion;
     float m_g;
 
-    float m_texScale;
+    float m_sampleSteps;
+    float m_randomOffsetStrength;
     float m_minDensity;
     float m_densityFactor;
+    float m_densityOffset;
+
+    float m_texScale;
     float m_worly1Weight;
     float m_worly2Weight;
     float m_worly3Weight;
@@ -34,18 +34,26 @@ Texture3D p_detailTexture   : register(t0);
 Texture3D p_shapeTexture    : register(t1);
 SamplerState p_sampler  : register(s0);
 
-float sampleCloud(float3 coord)
+float4 multiplyQuat(float4 q1, float4 q2)
 {
-    float3 newCoord = m_texScale * coord;
-    newCoord = newCoord - floor(newCoord);
-    float4 tex = p_detailTexture.Sample(p_sampler, newCoord);
-    float worly = m_worly1Weight * (1 - tex.y) + m_worly2Weight * (1 - tex.z) + m_worly3Weight * (1 - tex.w);
-    float res = tex.x * worly;
-    if (res < m_minDensity) {
-        res = 0;
-    }
-    res *= m_densityFactor;
-    return res;
+    float a = q1.x * q2.x - q1.y * q2.y - q1.z * q2.z - q1.w * q2.w;
+    float b = q1.x * q2.y + q1.y * q2.x + q1.z * q2.w - q1.w * q2.z;
+    float c = q1.x * q2.z - q1.y * q2.w + q1.z * q2.x + q1.w * q2.y;
+    float d = q1.x * q2.w + q1.y * q2.z - q1.z * q2.y + q1.w * q2.x;
+
+    return float4(a, b, c, d);
+}
+float4 conjugateQuat(float4 q)
+{
+    return float4(q.x, -q.y, -q.z, -q.w);
+}
+float3 rotateVector(float3 v, float4 rotation)
+{
+    float4 vQ = float4(0, v);
+    float4 conjRot = conjugateQuat(rotation);
+    float4 rotatedVQ = multiplyQuat(rotation, multiplyQuat(vQ, conjRot));
+
+    return float3(rotatedVQ.y, rotatedVQ.z, rotatedVQ.w);
 }
 
 struct Wall
@@ -63,30 +71,6 @@ struct PSInput
     float2 uv               : UV;
     Wall walls[6]           : WALLS;
 };
-
-float4 multiplyQuat(float4 q1, float4 q2)
-{
-    float a = q1.x * q2.x - q1.y * q2.y - q1.z * q2.z - q1.w * q2.w;
-    float b = q1.x * q2.y + q1.y * q2.x + q1.z * q2.w - q1.w * q2.z;
-    float c = q1.x * q2.z - q1.y * q2.w + q1.z * q2.x + q1.w * q2.y;
-    float d = q1.x * q2.w + q1.y * q2.z - q1.z * q2.y + q1.w * q2.x;
-
-    return float4(a, b, c, d);
-}
-
-float4 conjugateQuat(float4 q)
-{
-    return float4(q.x, -q.y, -q.z, -q.w);
-}
-
-float3 rotateVector(float3 v, float4 rotation)
-{
-    float4 vQ = float4(0, v);
-    float4 conjRot = conjugateQuat(rotation);
-    float4 rotatedVQ = multiplyQuat(rotation, multiplyQuat(vQ, conjRot));
-
-    return float3(rotatedVQ.y, rotatedVQ.z, rotatedVQ.w);
-}
 
 void generateWalls(out Wall walls[6], float3 center, float4 rotation, float3 extents)
 {
@@ -132,11 +116,10 @@ void generateWalls(out Wall walls[6], float3 center, float4 rotation, float3 ext
         walls[i].m_origin += center;
     }
 }
-
 bool intersectWall(Wall wall, float3 startPoint, float3 dir, out float3 intersection)
 {
     matrix mat = matrix(float4(normalize(wall.m_x), 0), float4(normalize(wall.m_y), 0), float4(normalize(wall.m_z), 0), float4(0, 0, 0, 1));
-    
+
     float3 dirTr = mul(mat, float4(dir, 0)).xyz;
     float3 startPointTr = mul(mat, float4(startPoint - wall.m_origin, 1)).xyz;
 
@@ -159,7 +142,6 @@ bool intersectWall(Wall wall, float3 startPoint, float3 dir, out float3 intersec
     intersection = startPoint + c * dir;
     return true;
 }
-
 int findIntersections(Wall walls[6], float3 startPoint, float3 dir, out float3 intersections[2])
 {
     int num = 0;
@@ -185,12 +167,72 @@ int findIntersections(Wall walls[6], float3 startPoint, float3 dir, out float3 i
     return num;
 }
 
+
+
 float randNoise(float3 value) {
     float3 smallValue = sin(value);
     float random = dot(smallValue, float3(12.9898, 78.233, 37.719));
     random = sin(random) * 143758.5453;
     random -= floor(random);
     return random;
+}
+float sampleCloud(float3 coord)
+{
+    float3 newCoord = m_texScale * coord;
+    newCoord = newCoord - floor(newCoord);
+    float4 tex = p_detailTexture.Sample(p_sampler, newCoord);
+    float worly = m_worly1Weight * (1 - tex.y) + m_worly2Weight * (1 - tex.z) + m_worly3Weight * (1 - tex.w);
+    float res = tex.x * worly;
+    if (res < m_minDensity) {
+        res = 0;
+    }
+    res *= m_densityFactor;
+    return res;
+}
+
+
+
+float phase(float g, float cosAngle)
+{
+    float g2 = g * g;
+    float x = 0.5 * (1 - g2) / pow(1 + g2 - 2 * g * cosAngle, 1.5);
+    float y = 0.5 * (1 - g2) / pow(1 + g2 + 2 * g * cosAngle, 1.5);
+    return 0.5 * (x + y);
+}
+float simpleScattering(float lightTransmittance, float angleCos)
+{
+    return phase(m_g, angleCos)
+        * exp(-lightTransmittance * m_cloudLightAbsorbtion)
+        * (1 - exp(-2 * lightTransmittance * m_cloudLightAbsorbtion));
+}
+float multipleOctaveScattering(float density, float angleCos)
+{
+    float attenuation = 0.2;
+    float contribution = 0.4;
+    float phaseAttenuation = 0.1;
+
+    const float scatteringOctaves = 4.0;
+
+    float a = 1.0;
+    float b = 1.0;
+    float c = 1.0;
+    float g = m_g;
+
+    float luminance = 0.0;
+
+    for (float i = 0.0; i < scatteringOctaves; ++i) {
+        float phaseFunction = phase(c * g, angleCos);
+        float beers = exp(-density * m_cloudLightAbsorbtion * a) *
+            (1 - exp(-2 * density * m_cloudLightAbsorbtion * a));
+
+        luminance += b * phaseFunction * beers;
+
+        a *= attenuation;
+        b *= contribution;
+        c *= (1 - phaseAttenuation);
+    }
+
+    return luminance;
 }
 
 float lightMarch(Wall walls[6], float3 pos, out float energy)
@@ -231,76 +273,6 @@ float lightMarch(Wall walls[6], float3 pos, out float energy)
     }
 
     return lightTransmittance;
-}
-
-float phase(float g, float cosAngle)
-{
-    float g2 = g * g;
-    float x = 0.5 * (1 - g2) / pow(1 + g2 - 2 * g * cosAngle, 1.5);
-    float y = 0.5 * (1 - g2) / pow(1 + g2 + 2 * g * cosAngle, 1.5);
-    return 0.5 * (x + y);
-}
-
-PSInput VSMain(
-    float3 position : POSITION,
-    float3 normal : NORMAL,
-    float2 uv : UV,
-    float3 objectPosition : OBJECT_POSITION,
-    float4 objectRotation : OBJECT_ROTATION,
-    float3 objectScale : OBJECT_SCALE)
-{
-    PSInput result;
-    float3 scaledPos = objectScale * position;
-    float3 rotatedPos = rotateVector(scaledPos, objectRotation);
-    float3 pos = objectPosition + rotatedPos;
-
-    result.position = mul(m_matrix, float4(pos, 1));
-    result.world_position = float4(pos, 1);
-    result.uv = uv;
-
-    Wall walls[6];
-    generateWalls(walls, objectPosition, objectRotation, objectScale);
-
-    result.walls = walls;
-
-    return result;
-}
-
-float simpleScattering(float lightTransmittance, float angleCos)
-{
-    return phase(m_g, angleCos)
-        * exp(-lightTransmittance * m_cloudLightAbsorbtion)
-        * (1 - exp(-2 * lightTransmittance * m_cloudLightAbsorbtion));
-}
-
-float multipleOctaveScattering(float density, float angleCos)
-{
-    float attenuation = 0.2;
-    float contribution = 0.4;
-    float phaseAttenuation = 0.1;
-
-    const float scatteringOctaves = 4.0;
-
-    float a = 1.0;
-    float b = 1.0;
-    float c = 1.0;
-    float g = m_g;
-
-    float luminance = 0.0;
-
-    for (float i = 0.0; i < scatteringOctaves; ++i) {
-        float phaseFunction = phase(c * g, angleCos);
-        float beers = exp(-density * m_cloudLightAbsorbtion * a) *
-            (1 - exp(-2 * density * m_cloudLightAbsorbtion * a));
-
-        luminance += b * phaseFunction * beers;
-
-        a *= attenuation;
-        b *= contribution;
-        c *= (1 - phaseAttenuation);
-    }
-
-    return luminance;
 }
 
 float2 cloudMarch(Wall walls[6], float3 hitPoint)
@@ -352,7 +324,30 @@ float2 cloudMarch(Wall walls[6], float3 hitPoint)
     return float2(light, exp(-transmittance * m_cloudAbsorbtion));
 }
 
+PSInput VSMain(
+    float3 position : POSITION,
+    float3 normal : NORMAL,
+    float2 uv : UV,
+    float3 objectPosition : OBJECT_POSITION,
+    float4 objectRotation : OBJECT_ROTATION,
+    float3 objectScale : OBJECT_SCALE)
+{
+    PSInput result;
+    float3 scaledPos = objectScale * position;
+    float3 rotatedPos = rotateVector(scaledPos, objectRotation);
+    float3 pos = objectPosition + rotatedPos;
 
+    result.position = mul(m_matrix, float4(pos, 1));
+    result.world_position = float4(pos, 1);
+    result.uv = uv;
+
+    Wall walls[6];
+    generateWalls(walls, objectPosition, objectRotation, objectScale);
+
+    result.walls = walls;
+
+    return result;
+}
 
 float4 PSMain(
     float4 position : SV_POSITION,
